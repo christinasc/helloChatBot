3a28f35debdfd4e6b05b07e334bfebb37b6dbd89
Christina <Christinasc@gmail.com>
2017-01-10 23:43:10 -0800

    update for viber only

--- bot.js
+++ bot.js
@@ -3,14 +3,17 @@ const viberTemplate = botBuilder.viberTemplate;
 
 module.exports = botBuilder(message => {
 	if (message.type === 'viber')
-	    return new viberTemplate.Url('https://claudiajs.com').get();
-	/*	    new viberTemplate.Text(`What\'s your favorite House in Game Of Thrones`)
-	 .addReplyKeyboard()
-        .addKeyboardButton('Stark', 'STARK', 6, 1)
-        .addKeyboardButton('Lannister', 'LANNISTER', 6, 1)
-        .addKeyboardButton('Targaryen', 'TARGARYEN', 6, 1)
-        .addKeyboardButton('None of the above', 'OTHER', 6, 1)
-      .get();
-	*/
+	    //	    return new viberTemplate.Url('https://claudiajs.com').get();
+    return [
+	    'Hello There! Ask me a question.',
+	    new viberTemplate.Text(`What\'s your favorite House in Game Of Thrones`)
+        .addReplyKeyboard()
+          .addKeyboardButton('Stark', 'STARK', 6, 1)
+          .addKeyboardButton('Lannister', 'LANNISTER', 6, 1)
+          .addKeyboardButton('Targaryen', 'TARGARYEN', 6, 1)
+          .addKeyboardButton('None of the above', 'OTHER', 6, 1)
+        .get()
+    ];
+	
 });
 
